a3a844e7f2eb75085fbc61d5baf570938eddf866
Aaron <79121360+UnrealApex@users.noreply.github.com>
2021-12-25 02:32:52 -0800

    Renamed variables

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ while True:
   print("Welcome to the random number guessing game\nTo play, enter a number between 1 and 100 and press enter.\n====================");
   # make sure the users enters a number
   try:
-    y = int(input("Enter your guess here: "))
-    print(y)
+    guess = int(input("Enter your guess here: "))
+    print(guess)
   # try again if a number isn't entered
   except ValueError:
     print("Enter a number!")
-    y = int(input("Enter your (number) guess here: "))
-    print(y)  
-  if x == y:
+    guess = int(input("Enter your (number) guess here: "))
+    print(guess)  
+  if x == guess:
     print("Congratulations, you guessed the number!")
   # if the user's guess was 10 numbers away, tell them they were close to the right guess
   else:
-    if (x == (y - 10)) or (x == (y + 10)):
+    if (x == (guess - 10)) or (x == (guess + 10)):
       print("You didn't guess the number, but you were very close\nThe number was " + str(x)) 
     else:  
       print("You didn't guess the number\nThe number was " + str(x))
   # ask the user if they want to play again, restart if if true, stop it if false 
-  z = str(input("\n\n\nWant to play again? (Y/n): ")).lower()
-  print(z)
-  if z == "y" or z == "yes":
+  play_again = str(input("\n\n\nWant to play again? (Y/n): ")).lower()
+  print(play_again)
+  if play_again == "y" or play_again == "yes":
     print("\nRestarting game...\n")
-  elif z == "n" or z == "no":
+  elif play_again == "n" or play_again == "no":
     print("Game stopped")
     break
   else:
